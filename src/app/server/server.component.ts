44811eb7265fd48c93c8373d80bbb25635492a52
prasaria <prasetya.ariawibawa@gmail.com>
2017-04-10 21:46:48 +0700

    ok

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent{
-
+    
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+    
 }
---
+++ src/app/success/success.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-success',
+    templateUrl: './success.component.html',
+    styleUrls: ['./success.component.css']
+})
+
+export class SuccessComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
